b834cb3709fa73f5626c8f2ee187ba17a5bb208c
B-star-star <84957139+B-star-star@users.noreply.github.com>
2023-05-11 12:44:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,35 @@
-prediction_1="";
 
+Harry_Potter_Theme_Song="";
+Peter_Pan="";
+LeftWristx=0;
+LeftWristY=0;
+RightWristX=0;
+RightWristY=0;
 
-Webcam.set
-({
-width:350,
-height:300,
-image_format:'png',
-png_quality:90
-});
+function setup(){
+  canvas=createCanvas(500,400);
+  canvas.center();
 
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
-
-function take_snapshot()
-{
-Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML='<img id="cptured_image" src="'+ data_uri+'"/>';
-});
+  video=createCapture(VIDEO);
+  video.hide();
+  poseNet=ml5.poseNet(video,modeLoaded)
+  pseNet.on('pose',gotPoses)
 }
 
-console.log('ml5 version:', ml5.version);
-
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4_b2p1SKB/model.json',model_loded);
-
-function model_loded()
-{
-    console.log('model is loaded');
+function modeLoaded(){
+  console.log("Posenet is Initialised");
 }
 
-function check(){
-    img=document.getElementById("captured_image");
-    classifier.classify(img,gotResult);
+function preload(){
+    Harry_Potter_Theme_Song =loadSound("music.mp3");
+    Peter_Pan =loadSound("music2.mp3");
 }
 
-function speak()
-{
-    var synth=window.speechSynthesis;
-    speak_data_1=tospeak;
-    var utterThis=new SpeechSynthesisUtterance(speak_data_1);
-    synth.speak(utterThis);
+function draw(){
+
+    image(video,0,0,500,400);
 }
 
-function gotResult(error,results){
-    if(error){console.error(error);
-    }
- else{
-     console.log(results);
-     
-     document.getElementById("result_guesture_name").innerHTML=results[0].label;
-     prediction_1=results[0].label;
-    
-   tospeak="";
-    if(results[0].label=="Victory"){
-        tospeak="victory";
-        document.getElementById("guesture_emoji").innerHTML="&#9996;";
-    }
-    if(results[0].label=="Nice"){
-        tospeak="nice";
-           document.getElementById("guesture_emoji").innerHTML="&#128076;";
-   }
-   if(results[0].label=="Hello"){
-    tospeak="hello";
-       document.getElementById("guesture_emoji").innerHTML="&#128075;";
-   }
-   
- 
- speak() ; 
+function gotPoses(){
   
-}
 }
